8094609e9241e78b4d5ec1bf1f1d849e0b46a6ef
Vitaliy Fadeev <vital.fadeev@gmail.com>
2023-09-29 15:23:27 +0700

    upd

--- source/vf/raster.d
+++ source/vf/raster.d
@@ -269,10 +269,10 @@ struct Raster
         {
             _color = RGBQUAD( 0, 255, 255, 0);
 
-            auto _bar2_x_inc = (bar2) * _x_inc;
+            auto _bar2_x_inc  = _x_inc * (bar2);
             auto _x_limit_inc = _y_inc   + _bar2_x_inc;
             auto _x_limit     = _current + _bar2_x_inc;
-            auto _y_limit     = _current + _x_limit_inc * bar2n;
+            auto _y_limit     = _current + _x_limit_inc * (bar2n);
 
             for ( ; _current != _y_limit; _current+=_y_inc, _x_limit+=_x_limit_inc )
                 for ( ; _current != _x_limit; _current+=_x_inc )
@@ -356,10 +356,10 @@ struct Raster
         {
             _color = RGBQUAD( 0, 255, 255, 0);
 
-            auto _bar2_y_inc  = (bar2) * _y_inc;
+            auto _bar2_y_inc  = _y_inc * (bar2);
             auto _y_limit_inc = _x_inc   + _bar2_y_inc;
             auto _y_limit     = _current + _bar2_y_inc;
-            auto _x_limit     = _current + _y_limit_inc * bar2n;
+            auto _x_limit     = _current + _y_limit_inc * (bar2n);
 
             for ( ; _current != _x_limit; _current+=_x_inc, _y_limit+=_y_limit_inc )
                 for ( ; _current != _y_limit; _current+=_y_inc )
