c47ed0b8078a2b13097ac3c038bc29a8d8442cd7
Vitaliy Fadeev <vital.fadeev@gmail.com>
2023-09-29 21:31:43 +0700

    upd

--- source/vf/platform/windows/game.d
+++ source/vf/platform/windows/game.d
@@ -35,7 +35,7 @@ import std.functional : toDelegate;
 import core.sys.windows.windows;
 import vf;
 
-class Game
+class Game(T)
 {
     static
     Queue   queue;
@@ -68,6 +68,18 @@ class Game
     Window new_window()
     {
         return new Window();            
+    }
+
+    static
+    T opCall()
+    {
+        static 
+        T _instance;
+
+        if ( _instance is null )
+            _instance = new T();
+
+        return _instance;
     }
 
     static
